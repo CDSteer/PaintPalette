97e8b1cebadef770d12440ed55b52784ac9fff79
CDSteer <cdsteer94@gmail.com>
2017-05-18 11:17:40 +0100

    code

---
+++ IOS10DrawShapesTutorial/Line.swift
@@ -0,0 +1,19 @@
+//
+//  Line.swift
+//  IOS10DrawShapesTutorial
+//
+//  Created by Cameron Steer on 16/05/2017.
+//  Copyright © 2017 Cameron Steer. All rights reserved.
+//
+
+import UIKit
+
+class Line{
+    var start: CGPoint
+    var end: CGPoint
+    
+    init(start _start: CGPoint, end _end: CGPoint) {
+        start = _start
+        end = _end
+    }
+}
